0bc62e54f62f0496b345405db78ed40d204673d3
Nauj Solrac <nauj1solrac@hotmail.com>
2023-11-01 23:55:24 -0400

    Fin seccion 8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [
+  ]
+})
+export class PromesasComponent implements OnInit{
+  
+  ngOnInit(): void {
+    this.getUsuarios().then(usuarios => {
+      console.log(usuarios);
+    })
+
+    // const promesa = new Promise((resolve, reject) => {
+    //   if(true){
+    //     resolve('Hola Mundo');
+    //   }else{
+    //     reject('Algo salio mal')
+    //   }
+    // });
+
+    // promesa.then((mensaje) => {
+    //   console.log(mensaje);
+    // })
+    // .catch(error => console.log('Error en mi promesa', error))
+
+    // console.log('Fin del Init')
+  }
+
+  getUsuarios(){
+    const promesa = new Promise((resolve) => {
+      fetch('https://reqres.in/api/users')
+      .then(response => response.json())
+      .then(body => resolve(body.data));
+    })
+
+    return promesa;
+  }
+}
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +9,28 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
+  public titulo!: string;
+  public tituloSubs$: Subscription;
 
+  constructor(private router: Router){
+    this.tituloSubs$ = this.getArgumentosRuta()
+    .subscribe(({titulo}) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    })
+  }
+  
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe()
+  }
+
+  getArgumentosRuta(){
+    return this.router.events
+    .pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd ) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd ) => event.snapshot.data)
+    )
+  }
 }
