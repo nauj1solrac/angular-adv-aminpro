9427ef629c139067888b482dade9f695369cdf1f
Nauj Solrac <nauj1solrac@hotmail.com>
2023-11-30 22:34:58 -0400

    Environment listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3005/api'
+  base_url: 'http://localhost:3005/api'     //colocar la url que el servidro te provea 
 };
